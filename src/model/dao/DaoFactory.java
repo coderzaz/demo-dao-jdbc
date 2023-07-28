13c70cc4daffc41ca48234b713cec5e2fb027f64
hall <coderzaz@gmail.com>
2023-07-28 15:51:22 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 //Esta classe vai ter operções estática para instânciar os Daos
 	//retorna um tipo da interface
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();//internamente instãncia uma implemetação
+		return new SellerDaoJDBC(DB.getConnection());//internamente instãncia uma implemetação,obrigado apassar uma conexão como argumento
 	}
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,12 +1,26 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao{
-
+	
+	private Connection conn;
+	//força a injeçãode dependencia
+	
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
@@ -27,9 +41,52 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+				    + "ON seller.DepartmentId = department.Id "
+					+ "WHERE seller.Id = ?");
+			
+			st.setInt(1,id); //recebe o id como parâmetro da função
+			rs = st.executeQuery(); //rs recebe a consulta sql
+			
+			//implemetação para retornar um vendedor por id
+			if(rs.next()) { //se for true
+				Department dep = new  Department(); //instânciamos um departamento
+				dep.setId(rs.getInt("DepartmentId")); //setamos os valores dele
+				dep.setName(rs.getString("DepName"));
+				
+				Seller obj = new Seller();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setEmail(rs.getString("Email"));
+				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+				obj.setBirthDate(rs.getDate("BirthDate"));
+				obj.setDepartment(dep); //dep é o objeto montado inteiro com toda a tabela
+				//ciramos o Objeto seller com todos os atributos definidos 
+				return obj;
+			}
+			return null; //caso a consulta seja nula
+		}
+		catch (SQLException e) { //cria uma exceção 
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+
 	}
+	
+	
+	
+	
+	
+	
+	
 
 	@Override
 	public List<Seller> findAll() {
