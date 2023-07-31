cb4ceb92218209e8d33448d40354d46855ab89d6
hall <coderzaz@gmail.com>
2023-07-31 17:06:30 -0300

    delete implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -8,9 +8,9 @@ import model.entities.Seller;
 public interface SellerDao {
 	void insert(Seller obj);
 	void update(Seller obj);
-	void deleteById(Seller  id);
-	
+	void deleteById(Integer  id);
 	Seller findById(Integer id);//consulta no Bd um Objeto com este id se existir retorna true ou null
+	
 	List<Seller> findAll();//Retorna todos os vendedores
 	List<Seller> findByDepartment(Department department);//assinatura do método,bscar por departamento
 }
